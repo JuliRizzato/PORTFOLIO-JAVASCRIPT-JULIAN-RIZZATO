c4e6ef5ae3f1117b8a5cf5a6386789fe0b9e6cb9
Julian Rizzato <161335571+JuliRizzato@users.noreply.github.com>
2025-10-27 10:54:22 -0300

    Add files via upload
    Modifiqué el archivo index.html y scipt.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Calculadora
+//1. Calculadora
 
 
 //Pedir los datos al usuario
@@ -31,7 +31,7 @@ if (operacion === '+'){
 console.log(`Resultado: ${resultado}`);
 
 
-//ADIVINA EL NÚMERO
+//2. ADIVINA EL NÚMERO
 
 const numeroSecreto = Math.floor(Math.random() * 20) + 1; //Número entre 1 y 10
 let intentos = 6;
@@ -60,9 +60,144 @@ let numeroT = parseInt(prompt('Ingrese un número para ver su tabla de multiplic
 let limite = parseInt(prompt('¿Hasta qué número quieres multiplicar?:'));
 
 console.log(`Tabla de multiplicar del ${numeroT}`);
+
 for (let i = 1; i <= limite; i++){
-   console.log(numeroT * i);
+   console.log(`${numeroT} x ${i} = ${numeroT * i}`);
 }
 
 
+
 //Desafío: Guarda los resultados en un array y muéstralos con un for...of
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//AGENDA DE CONTACTOS 
+
+let agenda = JSON.parse(localStorage.getItem('agenda')) || [];
+
+//Guardar cambios en localStorage
+function guardarAgenda(){
+    localStorage.setItem('agenda', JSON.stringify(agenda));
+}
+
+
+//Función para agregar contactos
+function agregarContacto(nombre, telefono, email){
+const contacto = {nombre, telefono, email};
+agenda.push(contacto);
+guardarAgenda();
+console.log(`Contacto agregado: ${nombre}`);
+};
+
+//Función para mostrar todos los contactos
+function mostrarAgenda(){
+console.log('Lista de contactos:');
+agenda.forEach(({nombre, telefono, email}, i) => {
+    console.log(`${i + 1}. ${nombre} - ${telefono} - ${email}`);
+});
+};
+
+//Función para buscar contacto
+function buscarContacto(nombre){
+    const contacto = agenda.find((c) => c.nombre.toLowerCase() === nombre.toLowerCase()
+);
+if (contacto){
+    console.log(`Encontrado: ${contacto.nombre} - ${contacto.telefono}`);
+} else {
+    console.log('Contacto no encotrado');
+}};
+
+//Función para eliminar contacto
+function eliminarContacto(nombre){
+    agenda = agenda.filter(
+        (c) => c.nombre.toLowerCase() !== nombre.toLowerCase()
+    );
+    guardarAgenda();
+    console.log(`Contacto elimninado: ${nombre}`);
+}
+
+
+//Prograna principal(ejemplo)
+agregarContacto('Ana', '261-1234567', 'ana@gmial.com');
+agregarContacto('Luis', '261-7654321', 'luis@gmail.com');
+mostrarAgenda();
+
+buscarContacto('Ana');
+eliminarContacto('Ana');
+buscarContacto('Ana');
+
+
+
+
+let segundos = 0;
+let intervalo;
+function actualizarCronometro(){
+    const hrs = String(Math.floor(segundos / 3600)).padStart(2, '0');
+    const mins = String(Math.floor(segundos % 3600 / 60)).padStart(2, '0');
+    const secs = String(segundos % 60).padStart(2, '0');
+
+    document.getElementById('cronometro').textContent = `${hrs}:${mins}:${secs}`;
+}
+
+document.getElementById('iniciar').addEventListener('click', () => {
+  if (!intervalo){
+    intervalo = setInterval(() => {
+        segundos++;
+        actualizarCronometro();
+    }, 1000);
+  }
+});
+
+document.getElementById('pausar').addEventListener('click', () => {
+     clearInterval(intervalo);
+    intervalo = null;
+});
+
+document.getElementById('reiniciar').addEventListener('click', () => {
+    segundos = 0;
+    actualizarCronometro();
+    clearInterval(intervalo);
+    intervalo = null;
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
